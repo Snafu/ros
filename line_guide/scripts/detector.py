a5cf27afe190f1d4171b181bf8510981e419f74c
Christopher Gabriel <stuff@c-gabriel.at>
2013-11-20 17:04:27 +0100

    ROV is driving

--- line_guide/scripts/detector.py
+++ line_guide/scripts/detector.py
@@ -5,6 +5,8 @@ import sys
 import rospy
 import cv2
 import numpy as np
+from math import copysign
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -15,13 +17,24 @@ class Detector:
     self.win = 'Line Detector'
     self.opt = 'Options'
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber('camera/image_raw', Image, self.handlecb)
+    self.image_sub = rospy.Subscriber('camera/image_raw', Image, self.handlecb, None, 1)
     self.color_pub = rospy.Publisher('color_image', Image)
     self.mask_pub = rospy.Publisher('mask_image', Image)
+    self.cmd_vel = rospy.Publisher('RosAria/cmd_vel', Twist)
 
     #cv2.namedWindow(self.win)
-    self.hsv_min = [100, 60, 30]
-    self.hsv_max = [150, 255, 255]
+    self.hsv_min = [0, 150, 50]
+    self.hsv_max = [10, 255, 255]
+
+    self.follow_line = False
+    self.move = False
+    self.last_move = False
+    self.rotAdjust = 0.1
+    self.accAdjust = 0.2
+    self.lcx = 0
+    self.lcy = 0
+    self.cxAvg = np.zeros(5)
+    self.cyAvg = np.zeros(5)
 
     self.showOptions()
   def hsvH_min(self, val):
@@ -57,6 +70,10 @@ class Detector:
       return
     color = np.asarray(color)
     color = cv2.medianBlur(color, 9)
+    img_h, img_w, bpp = color.shape
+    # clip image to bottom half
+    color = color[img_h/2:, :]
+    img_h, img_w, bpp = color.shape
     
     # convert to HSV
     mask = cv2.cvtColor(color, cv2.cv.CV_BGR2HSV)
@@ -80,9 +97,33 @@ class Detector:
     # calculate center of gravity position
     if max_cont is not None:
       M = cv2.moments(max_cont)
-      cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-      cv2.circle(color, (cx,cy), 5, (0,0,255), -1)
+      ccx, ccy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+      self.cxAvg, cx = self.runningAvg(self.cxAvg, ccx)
+      self.cyAvg, cy = self.runningAvg(self.cyAvg, ccy)
+      lcx, lcy = float(cx)/img_w - 0.5, float(img_h - cy)/img_h
+
+      cv2.circle(color, (ccx,ccy), 3, (0,0,255), -1)
+      cv2.circle(color, (int(cx),int(cy)), 5, (255,0,0), -1)
       cv2.drawContours(color, [max_cont], -1, (0,255,0), 5)
+
+      t = Twist()
+      self.move = False
+      if self.follow_line and not -0.02 <= lcx <= 0.02:
+        t.angular.z = copysign(0.5, lcx) * -1 * self.rotAdjust
+        self.move = True
+
+      if self.follow_line and lcy > 0.1:
+        t.linear.x = max(lcy, 0.5) * self.accAdjust
+        self.move = True
+
+
+      # check if we should drive automatically
+      if self.move or (self.last_move and not self.move):
+        self.cmd_vel.publish(t)
+      
+      self.last_move = self.move
+      
+      rospy.loginfo("Center of gravity at %.2f, %.2f", lcx, lcy )
 
     try:
       tmp = cv2.cv.fromarray(mask)
@@ -101,10 +142,19 @@ class Detector:
 
     # menu
     key = cv2.waitKey(10)
+
+    if key == ord('k'):
+      self.follow_line = not self.follow_line
     """
     if key == ord('o'):
       self.showOptions()
     """
+
+  def runningAvg(self, l, i): 
+    l = np.roll(l, 1)
+    l[0] = i 
+    return l, np.convolve(l, np.ones(l.shape)/l.shape[0], 'valid')[0]
+
 
   def showOptions(self):
     cv2.namedWindow(self.opt)
@@ -117,8 +167,8 @@ class Detector:
 
 
 def main(args):
-  d = Detector()
   rospy.init_node('detector')
+  d = Detector()
   try:
     rospy.spin()
   except KeyboardInterrupt:
--- line_guide/scripts/vc-c50i.sh
+++ line_guide/scripts/vc-c50i.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Using v4l2-ctl from ivtv-utils to setup the camera feed
 # Image messages are published via gscam
-v4l2-ctl -v width=320,height=240,pixelformat=6
 v4l2-ctl -s ntsc
+v4l2-ctl -v width=320,height=240,pixelformat=BGR3
